682fc7785e24e61639ddc15e0fe94dd201a35851
Swati Sharma <swati@headerlabs.com>
2017-03-21 01:55:48 +0530

    working on detail page

--- src/app/details/detail.ts
+++ src/app/details/detail.ts
@@ -7,7 +7,7 @@
 
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router'
-
+import {data} from '../mock_data/mock_data'
 
 
 @Component({
@@ -16,16 +16,24 @@ import {ActivatedRoute} from '@angular/router'
   styleUrls: ['./detail.css'],
 
 })
-export class DetailComponent implements OnInit{
- constructor( private route:ActivatedRoute){
+export class DetailComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {
+  }
+
+  detail: {}
+  id = null;
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.detail = data;
+
+    for (let i=0; i<=data.length;i++) {
+
+      console.log("data",data[i]);
 
 
- }
- id=null;
-   ngOnInit():void{
-   this.id=this.route.snapshot.params['id'];
-   console.log("id",this.id)
-   }
+    }
 
 
+  }
 }
